b556522e56f3bc8d3e019e14c64640428a5b6885
vandana414 <vandanakudigrama.shenoy@sjsu.edu>
2019-04-25 17:21:48 -0700

    Logistic Regression model

---
+++ code/LogisticRegression.py
@@ -0,0 +1,52 @@
+from sklearn.preprocessing import Normalizer
+from sklearn.linear_model import LogisticRegression
+from joblib import dump,load
+import numpy as np
+import pandas as pd
+import sys
+sys.path.append("../")
+from preprocessing_scripts.handlecategorical import categories
+from preprocessing_scripts.labels import labels
+
+class LRModel:
+    normalizer = Normalizer(norm ='l2')
+    model = LogisticRegression(C=1000,penalty='l2',random_state=0, solver='liblinear',multi_class='ovr')
+   
+       
+    def fitNormalizer(self,train):
+        self.normalizer.fit(train)
+    
+    def normalize(self,test):
+        return self.normalizer.transform(test)
+        
+    
+    def trainModel(self,train,y):
+        train = categories.createCategorical(train)
+        self.fitNormalizer(train)
+        train = self.normalize(train)
+        self.model.fit(train, y)
+    
+    def predict(self,test):
+        test = categories.createCategorical(test)
+        test1 = self.normalize(test)
+        return self.model.predict(test1).tolist()
+    
+    def save(self,f_name):
+        dump(self.normalizer, f_name+'_normalizer.joblib')
+        dump(self.model,f_name+'_model.joblib')
+        
+    def load_model(self,f_name):
+        self.normalizer = load(f_name+'_normalizer.joblib')
+        self.model = load(f_name+'_model.joblib')
+        
+if __name__ == '__main__':
+    #read train data from train.csv
+    train_df = pd.read_csv('../data/train.csv',usecols = ['STAT_CAUSE_DESCR','LATITUDE','LONGITUDE','DISCOVERY_DATE','FIRE_SIZE'])
+    y = pd.DataFrame()
+    y['STAT_CAUSE_DESCR']=train_df['STAT_CAUSE_DESCR']
+    y=labels.createLabel(y)
+    y=y['STAT_CAUSE_DESCR'].astype(int)
+    train_df = train_df.drop(columns=['STAT_CAUSE_DESCR'])
+    model = LRModel()
+    model.trainModel(train_df,y)
+    model.save("../models/lrmodel")
---
+++ preprocessing_scripts/labels.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+class labels:
+    def createLabel(labels):
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Arson']=0
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Lightning']=1
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Debris Burning']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Campfire']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Children']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Fireworks']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Powerline']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Railroad']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Smoking']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Structure']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Equipment Use']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Miscellaneous']=4
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Missing/Undefined']=5
+        return labels
