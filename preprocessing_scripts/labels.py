01da11cbc64796726e46d27e5f3c5a2a02169c7e
vandana414 <vandanakudigrama.shenoy@sjsu.edu>
2019-04-30 12:43:29 -0700

    fixing labels issue

--- preprocessing_scripts/labels.py
+++ preprocessing_scripts/labels.py
@@ -7,14 +7,14 @@ class labels:
         labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Arson']=0
         labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Lightning']=1
         labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Debris Burning']=2
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Campfire']=3
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Children']=3
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Fireworks']=3
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Powerline']=3
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Railroad']=3
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Smoking']=3
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Structure']=3
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Equipment Use']=3
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Miscellaneous']=4
-        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Missing/Undefined']=4
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Campfire']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Children']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Fireworks']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Powerline']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Railroad']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Smoking']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Structure']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR =='Equipment Use']=2
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Miscellaneous']=3
+        labels['STAT_CAUSE_DESCR'][labels.STAT_CAUSE_DESCR == 'Missing/Undefined']=3
         return labels
