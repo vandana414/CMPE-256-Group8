09a37064c702b0e41a0f054ad89f56e50b594a53
Deepti Srinivasan <deeptisrini@Deeptis-MacBook-Pro.local>
2019-04-29 17:56:20 -0700

    Added all columns

--- code/runmodels.py
+++ code/runmodels.py
@@ -65,7 +65,7 @@ def preprocess(firedata):
 def read_data():
 
     conn = sqlite3.connect('../data/FPA_FOD_20170508.sqlite')
-    df = pd.read_sql("SELECT * from Fires LIMIT 1000",con=conn)
+    df = pd.read_sql("SELECT * from Fires",con=conn)
     firedata = df.filter(['SOURCE_REPORTING_UNIT_NAME','STATE','LATITUDE','LONGITUDE','FIRE_SIZE','FIRE_SIZE_CLASS','FIRE_YEAR','DISCOVERY_DATE','STAT_CAUSE_DESCR','CONT_DATE','CONT_DOY','CONT_TIME','OWNER_CODE','COUNTY'],axis=1)
     return firedata
 
