43be4fd2a1b97d87f8674b3953dde2cc2278d371
Deepti Srinivasan <deeptisrini@Deeptis-MacBook-Pro.local>
2019-05-01 10:15:40 -0700

    Added smaller subset for Data fires.csv

--- preprocessing_scripts/readdata.py
+++ preprocessing_scripts/readdata.py
@@ -5,6 +5,6 @@ import sqlite3
 class read_sql:
     def read_data():
 
-        df = pd.read_csv('../data/fires.csv',nrows=1000 ,sep=',', error_bad_lines=False, index_col=False, dtype='unicode')
+        df = pd.read_csv('../data/fires.csv' ,sep=',', error_bad_lines=False, index_col=False, dtype='unicode')
         firedata = df[['SOURCE_REPORTING_UNIT_NAME','STATE','LATITUDE','LONGITUDE','FIRE_SIZE','FIRE_SIZE_CLASS','FIRE_YEAR','DISCOVERY_DATE','STAT_CAUSE_DESCR','CONT_DATE','CONT_DOY','CONT_TIME','OWNER_CODE','COUNTY']]
         return firedata
